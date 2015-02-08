6ee192a4f673b9e255138e51918bcc7f07466f49
pf <peter.john.farrell@gmail.com>
2015-02-08 20:42:12 +0000

    Added a method "letsMakeChanges".

--- src/com/test/GoodbyeWorld.java
+++ src/com/test/GoodbyeWorld.java
@@ -4,6 +4,11 @@ public class GoodbyeWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Goodbye world!");
+		letsMakeChanges();
+	}
+
+	private static void letsMakeChanges() {
+		System.out.println("Changes we can believe in!");
 	}
 
 }
